538d58a6164a93a98f4a931c02ebfba5a2f2563f
lkito <45845329+lkito@users.noreply.github.com>
2020-11-23 04:48:50 +0400

    Some progress on index page

--- script.js
+++ script.js
@@ -15,3 +15,46 @@ scrollerImgRight.onclick = function(){
     scrollerImgCenter.src = scrollerImgRight.src;
     scrollerImgRight.src = temp;
 };
+
+
+const ele = document.getElementById('new-blogs');
+ele.style.cursor = 'pointer';
+
+let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+const mouseDownHandler = function(e) {
+    ele.style.cursor = 'grabbing';
+    ele.style.userSelect = 'none';
+
+    pos = {
+        left: ele.scrollLeft,
+        top: ele.scrollTop,
+        // Get the current mouse position
+        x: e.clientX,
+        y: e.clientY,
+    };
+
+    ele.addEventListener('mousemove', mouseMoveHandler);
+    ele.addEventListener('mouseup', mouseUpHandler);
+};
+
+const mouseMoveHandler = function(e) {
+    // How far the mouse has been moved
+    const dx = e.clientX - pos.x;
+    const dy = e.clientY - pos.y;
+
+    // Scroll the element
+    ele.scrollTop = pos.top - dy;
+    ele.scrollLeft = pos.left - dx;
+};
+
+const mouseUpHandler = function() {
+    ele.style.cursor = 'pointer';
+    ele.style.removeProperty('user-select');
+
+    ele.removeEventListener('mousemove', mouseMoveHandler);
+    ele.removeEventListener('mouseup', mouseUpHandler);
+};
+
+// Attach the handler
+ele.addEventListener('mousedown', mouseDownHandler);
