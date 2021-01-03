140ae576aadec8deca45354eaa46758e2709b517
lkito <45845329+lkito@users.noreply.github.com>
2021-01-03 18:18:54 +0400

    update index script

--- scripts/index_script.js
+++ scripts/index_script.js
@@ -16,56 +16,73 @@ scrollerImgRight.onclick = function(){
     scrollerImgRight.src = temp;
 };
 
+const addScroller = function(elem) {
+    let pos = { top: 0, left: 0, x: 0, y: 0 };
 
-const ele = document.getElementById('new-blogs');
-ele.style.cursor = 'pointer';
-
-let pos = { top: 0, left: 0, x: 0, y: 0 };
-
-const mouseDownHandler = function(e) {
-    ele.style.cursor = 'grabbing';
-    ele.style.userSelect = 'none';
-
-    pos = {
-        left: ele.scrollLeft,
-        top: ele.scrollTop,
-        // Get the current mouse position
-        x: e.clientX,
-        y: e.clientY,
+    const mouseDownHandler = function(e) {
+        elem.style.cursor = 'grabbing';
+        elem.style.userSelect = 'none';
+    
+        pos = {
+            left: elem.scrollLeft,
+            top: elem.scrollTop,
+            // Get the current mouse position
+            x: e.clientX,
+            y: e.clientY,
+        };
+    
+        elem.addEventListener('mousemove', mouseMoveHandler);
+        elem.addEventListener('mouseup', mouseUpHandler);
     };
 
-    ele.addEventListener('mousemove', mouseMoveHandler);
-    ele.addEventListener('mouseup', mouseUpHandler);
+    const mouseMoveHandler = function(e) {
+        // How far the mouse has been moved
+        const dx = e.clientX - pos.x;
+        const dy = e.clientY - pos.y;
+    
+        // Scroll the element
+        elem.scrollTop = pos.top - dy;
+        elem.scrollLeft = pos.left - dx;
+    };
+
+    const mouseUpHandler = function() {
+        elem.style.cursor = 'pointer';
+        elem.style.removeProperty('user-select');
+    
+        elem.removeEventListener('mousemove', mouseMoveHandler);
+        elem.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    // Attach the handler
+    elem.addEventListener('mousedown', mouseDownHandler);
+}
+
+const blog_elem = document.getElementById('new-blogs');
+const tweet_elem = document.getElementById('new-tweets');
+addScroller(blog_elem);
+addScroller(tweet_elem);
+
+
+
+const ScrollRight = function(elem) {
+    elem.scrollLeft += 200;
 };
 
-const mouseMoveHandler = function(e) {
-    // How far the mouse has been moved
-    const dx = e.clientX - pos.x;
-    const dy = e.clientY - pos.y;
-
-    // Scroll the element
-    ele.scrollTop = pos.top - dy;
-    ele.scrollLeft = pos.left - dx;
+const ScrollLeft = function(elem) {
+    elem.scrollLeft -= 200;
 };
 
-const mouseUpHandler = function() {
-    ele.style.cursor = 'pointer';
-    ele.style.removeProperty('user-select');
+document.getElementById('right-arrow-blogs').addEventListener('click', function(){
+    ScrollRight(blog_elem);
+});
+document.getElementById('left-arrow-blogs').addEventListener('click', function(){
+    ScrollLeft(blog_elem);
+});
 
-    ele.removeEventListener('mousemove', mouseMoveHandler);
-    ele.removeEventListener('mouseup', mouseUpHandler);
-};
 
-// Attach the handler
-ele.addEventListener('mousedown', mouseDownHandler);
-
-const ScrollRight = function(e) {
-    ele.scrollLeft += 200;
-};
-
-const ScrollLeft = function(e) {
-    ele.scrollLeft -= 200;
-};
-
-document.getElementById('right-arrow-blogs').addEventListener('click', ScrollRight);
-document.getElementById('left-arrow-blogs').addEventListener('click', ScrollLeft);
+document.getElementById('right-arrow-tweets').addEventListener('click', function(){
+    ScrollRight(tweet_elem);
+});
+document.getElementById('left-arrow-tweets').addEventListener('click', function(){
+    ScrollLeft(tweet_elem);
+});
