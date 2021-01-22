2b1a574798c67da4640c95ec2e160c182e61dab9
lkito <45845329+lkito@users.noreply.github.com>
2021-01-22 07:13:23 +0400

    Adds regex matching to routes

--- WebFinalApp/WebFinalApp/wwwroot/scripts/router.js
+++ WebFinalApp/WebFinalApp/wwwroot/scripts/router.js
@@ -8,25 +8,38 @@ const blogs = new Blogs();
 
 const appElem = document.getElementById('app'); 
 
+const pathToRegex = (path) => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+const getParams = match => {
+    if (match.result === []) return [];
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+    return Object.fromEntries(keys.map((key, i) => {
+        return [key, values[i]];
+    }));
+};
+
+
+
 // uri : filePath
 const routeMaps = {
     '/': {
         filePath: '/index.html',
-        callBack: () => {
+        callBack: (params) => {
             index.indexLoad();
         },
         unload: () => { }
     },
     '/index': {
         filePath: '/index.html',
-        callBack: () => {
+        callBack: (params) => {
             index.indexLoad();
         },
         unload: () => { }
     },
     '/gallery': {
         filePath: '/gallery.html',
-        callBack: () => {
+        callBack: (params) => {
             gallery.galleryLoad();
         },
         unload: () => {
@@ -35,16 +48,16 @@ const routeMaps = {
     },
     '/blogs': {
         filePath: '/blogs.html',
-        callBack: () => {
+        callBack: (params) => {
             blogs.blogsLoad();
         },
         unload: () => {
             blogs.blogsUnload();
         }
     },
-    '/article': {
+    '/article/:id': {
         filePath: '/article.html',
-        callBack: () => { },
+        callBack: (params) => { console.log(params); },
         unload: () => { }
     },
 };
@@ -52,17 +65,35 @@ const routeMaps = {
 let unloadFunc;
 
 const loadContent = (uri) => {
-    const callBack = routeMaps[uri].callBack;
+    let match;
+    for (const [route] of Object.entries(routeMaps)) {
+        const curMatch = uri.match(pathToRegex(route));
+        if (curMatch !== null) {
+            match = {
+                route,
+                result: curMatch
+            };
+            break;
+        }
+    }
+    if (match === undefined) {
+        match = {
+            route: '/',
+            result: []
+        };
+    }
+
+    const callBack = routeMaps[match.route].callBack;
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState === this.DONE && this.status < 400) {
             appElem.innerHTML = this.responseText;
-            callBack();
+            callBack(getParams(match));
         };
     };
-    xhttp.open('GET', routeMaps[uri].filePath, true);
+    xhttp.open('GET', routeMaps[match.route].filePath, true);
     xhttp.send();
-    unloadFunc = routeMaps[uri].unload;
+    unloadFunc = routeMaps[match.route].unload;
 };
 
 window.goTo = (uri, routeName) => {
